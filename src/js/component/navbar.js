7771a8e3b3a5ea6f8896014e100513a7638fedfe
jgustavoj <gjime011@fiu.edu>
2021-01-05 20:35:31 +0000

    initial commit

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -4,14 +4,12 @@ import { Link } from "react-router-dom";
 export const Navbar = () => {
 	return (
 		<nav className="navbar navbar-light bg-light mb-3">
-			<Link to="/">
-				<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-			</Link>
-			<div className="ml-auto">
-				<Link to="/demo">
-					<button className="btn btn-primary">Check the Context in action</button>
-				</Link>
-			</div>
+			<span>Gus Jimenez</span>
+			<span>Home</span>
+			<span>About</span>
+			<span>Projects</span>
+			<span>Resume</span>
+			<span>Contact</span>
 		</nav>
 	);
 };
