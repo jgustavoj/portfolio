1326e0236f56e91b56f70c7701ef6d1f66427190
jgustavoj <gjime011@fiu.edu>
2021-01-10 07:24:17 +0000

    styles

--- src/js/component/about.js
+++ src/js/component/about.js
@@ -5,34 +5,35 @@ import resize from "../../img/resize.jpg";
 export const About = () => {
 	return (
 		<>
-			<div className="content-fluid row mt-5" id="about">
+			<div className="content row" id="about">
 				<div className="col-8" id="intro">
-					<span style={{ fontSize: "4.8vw" }}>
+					<p style={{ fontSize: "4.8vw" }}>
 						Hello, <br />
 						My name is Gustavo, <br />
 						some people call me Gus, <br />
-						others “Goose” and I’m a Full Stack Developer.
-					</span>
+						others “Goose” and I’m a <u>Full Stack Developer.</u>
+					</p>
 				</div>
-				<div className="col" style={{ color: "darkBlue", fontSize: "1.1vw" }}>
+
+				{/* animated fade in arrow in between these 2 divs going towards the icons */}
+
+				<div className="col" style={{ /*color: "darkBlue",*/ fontSize: "1.1vw", marginTop: "30px" }}>
 					<i className="fab fa-linkedin-in fa-2x" />
 					<i className="fab fa-github fa-2x" />
 					<i className="fab fa-twitter fa-2x" />
 				</div>
 			</div>
-
-			{/* <div className="container">
-				<div className="row justify-content-center">
-					<div className="col-6 col-md-2">1 of 3</div>
-					<div className="col-6 col-md">
-						<img src={image} className="img-fluid" alt="about" />{" "}
-					</div>
-					<div className="col-6 col-md-2">3 of 3</div>
-				</div>
-			</div> */}
 			<div className="">
 				{/* this image needs to be wide  */}
 				<img src={resize} className="img-fluid" alt="about" />{" "}
+			</div>
+			<div className="mt-5">
+				<span style={{ fontSize: "3vw" }}>So, a little bit about me...</span>
+				<p style={{ fontSize: "1.6vw" }}>
+					{
+						"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labor et dolore magna aliqua."
+					}
+				</p>
 			</div>
 		</>
 	);
